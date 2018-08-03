a5b2cf4a66fe23f0b74a62086df7aab0af5fd622
thepolm3 <thepolm3@gmail.com>
2018-08-04 00:39:29 +0100

    settings
    Add settings.txt functionality

--- README.md.txt
+++ README.md.txt
@@ -2,3 +2,5 @@
 Takes every image in the _input_ folder and overlays the image 'overlay.png' onto them. Note this requires the overlay to have transparency (alpha).
 
 Simply download and run overlayImage.exe after storing some images in Input/
+
+You can edit settings.txt to change how the program behaves
--- overlayImage.py
+++ overlayImage.py
@@ -3,27 +3,61 @@ from PIL import Image
 
 allowed_exts = ['.png','.jpg']
 
-def getFileContaining(string, path = None):
-	for filename in os.listdir(path):
-	    if string in filename:
-	        break
-	else:
-	    return None
-	return filename
+settings = {
+	'INPUT_FOLDER': 'Input',
+	'OUTPUT_FOLDER': 'Output',
+	'OVERLAY_IMAGE': 'overlay.png',
+	'FIT_TO_SOURCE': True,
+	'SIZE': False
+}
 
-overlay = Image.open(getFileContaining('overlay'))
+with open('settings.txt','r') as f:
+	for line in f.readlines():
+		if line.replace(' ','')[0] in ['\n','#']: continue
+		key, value = line.replace(' ','').replace('\n','').split('#')[0].split('=')
+
+		if key == '':
+			continue
+
+		if value.lower() == 'true':
+			value = True
+
+		elif value.lower() == 'false':
+			value = False
+
+		else:
+			try:
+				value = eval(value)
+
+			except Exception:
+				pass
+
+		settings[key] = value
+
+
+if not os.path.exists(settings['INPUT_FOLDER']): os.makedirs(settings['INPUT_FOLDER'])
+if not os.path.exists(settings['OUTPUT_FOLDER']): os.makedirs(settings['OUTPUT_FOLDER'])
+
+overlay = Image.open(settings['OVERLAY_IMAGE'])
 overlay = overlay.convert("RGBA")
 
-for filename in os.listdir('Input'):
+for filename in os.listdir(settings['INPUT_FOLDER']):
 	if os.path.splitext(filename)[1] not in allowed_exts:
 		continue
 	print(filename)
 
-	background = Image.open(f'Input/{filename}')
+	background = Image.open(f'{settings["INPUT_FOLDER"]}/{filename}')
 	background = background.convert("RGBA")
 
-	w, h = background.size
-	foreground = overlay.resize((w, h), Image.ANTIALIAS)  
+	if settings['SIZE']:
+		w, h = settings['SIZE']
+	elif settings['FIT_TO_SOURCE'] == True:
+		w, h = background.size
+	else:
+		w, h = overlay.size
+
+	foreground = overlay.resize((w, h), Image.ANTIALIAS)
+	background = background.resize((w, h), Image.ANTIALIAS)
 
 	background.paste(foreground, (0, 0), foreground)
-	background.save(f'Output/{filename}',"PNG")
+	background.save(f'{settings["OUTPUT_FOLDER"]}/{filename}',"PNG")
